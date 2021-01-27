fe8cb805d16d96778a48f1324215cef275980861
Michelle-Ward <Mward352@gmail.com>
2021-01-27 10:22:08 -0800

    create server and index.html

--- server/index.js
+++ server/index.js
@@ -2,21 +2,47 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const bodyParser = require('body-parser');
 
 
 const app = express();
 const port = 8080;
 
-app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname, '../public/')));
-//target carousel proxy
-app.use('/carousel', createProxyMiddleware({
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../public')));
+
+
+app.use('/api/new', createProxyMiddleware({
   target: 'http://localhost:3001',
   changeOrigin: true,
-  pathRewrite: {
-    [`^/carousel`]: '',
-},
 }));
+app.use('/api/homes', createProxyMiddleware({
+  target: 'http://localhost:3001',
+  changeOrigin: true,
+}));
+app.use('/api/similar', createProxyMiddleware({
+  target: 'http://localhost:3001',
+  changeOrigin: true,
+}));
+app.use('/api/photos', createProxyMiddleware({
+  target: 'http://localhost:3001',
+  changeOrigin: true,
+}));
+app.use('/api/properties', createProxyMiddleware({
+  target: 'http://localhost:3001',
+  changeOrigin: true,
+}));
+app.use('/api/all_rates', createProxyMiddleware({
+  target: 'http://localhost:3001',
+  changeOrigin: true,
+}));
+app.use('/api/home_price', createProxyMiddleware({
+  target: 'http://localhost:3001',
+  changeOrigin: true,
+}));
+
+
 
 
 app.listen(port, (err) => {
@@ -25,4 +51,4 @@ app.listen(port, (err) => {
   } else {
     console.log(`Server is now listening at ${port}`);
   }
-})
+});
